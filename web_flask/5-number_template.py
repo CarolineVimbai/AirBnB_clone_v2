723417cf197596e97e07f938db4cc932c68eadd0
CarolineVimbai <carolinevimbaichamboko@gmail.com>
2023-06-19 12:48:02 +0000

    Task 5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,50 +1,50 @@
 #!/usr/bin/python3
-"""Write a script that starts a Flask web application"""
-
-from flask import Flask
+"""hbnb flask module setup"""
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+app.url_map.strict_slashes = False
 
-@app.route("/", strict_slashes=False)
-def main():
-    """returns text"""
+
+@app.route('/')
+def home():
+    """return a string at the root page"""
     return "Hello HBNB!"
 
 
-@app.route("/hbnb", strict_slashes=False)
-def route_hbnb():
-    """text"""
+@app.route('/hbnb')
+def hbnb():
+    """return a string at the /hbnb page"""
     return "HBNB"
 
 
-@app.route('/c/<text>', strict_slashes=False)
+@app.route('/c/<text>')
 def c_is_fun(text):
-    """to print /c"""
-    text = text.replace("_", " ")
+    """return a string with C starting"""
+    text = text.replace('_', ' ')
     return "C {}".format(text)
 
 
-@app.route("/python", defaults={'text': "is cool"}, strict_slashes=False)
-@app.route("/python/<text>", strict_slashes=False)
-def python_display(text):
-    """Display “Python ”"""
-    return "".join(["Python ", text.replace("_", " ")])
+@app.route('/python')
+@app.route('/python/<text>')
+def python_is_cool(text='is cool'):
+    """return a string with Python starting,
+    default text is set to 'is cool'"""
+    return "Python {}".format(text.replace('_', ' '))
 
 
-@app.route("/number/<int:n>", strict_slashes=False)
-def n_is_integer(n):
-    """text"""
-    if type(n) is int:
-        return "{} is a number".format(n)
+@app.route('/number/<int:n>/')
+def is_number(n):
+    """detect arg passed is number"""
+    return "{} is a number".format(n)
 
 
-@app.route("/number_template/<int:n>", strict_slashes=False)
-def n_is_integer_html(n):
-    """text"""
-    if type(n) is int:
-        return render_template("5-number.html", number=n)
+@app.route('/number_template/<int:n>/')
+def number_temp(n):
+    """detect arg passed is number and render a html file"""
+    return render_template('5-number.html', n=n)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host='0.0.0.0', port=5000)
